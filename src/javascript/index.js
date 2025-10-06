254ed0365cb64b250fa6d23d7d2ce75b58daf478
jabo arnold landry <arnoldjab@gmail.com>
2025-10-06 18:12:43 +0200

    feat: add-search-funcitonality
    I added the functionality to the search input and handled different APIs calls for getting the longitude and latitude of the typed in location and also the function to fetch the weather information from that longitude and latitude

---
+++ src/javascript/index.js
@@ -0,0 +1,47 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+async function getLocation(location) {
+  try {
+    const response = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=1&language=en&format=json`
+    );
+    const data = await response.json();
+    return {
+      country: data.results[0].country,
+      city: data.results[0].name,
+      lat: data.results[0].latitude,
+      lon: data.results[0].longitude,
+    };
+  } catch (err) {
+    console.log("something bad happened: ", err);
+  }
+}
+async function weatherInformation(location) {
+  try {
+    const { lat, lon } = await getLocation(location);
+    const response = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,is_day,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min`
+    );
+    const weatherData = await response.json();
+    return {
+      "feels like": [
+        weatherData.current.temperature_2m,
+        weatherData.current_units.temperature_2m,
+      ],
+      humidity: [
+        weatherData.current.relative_humidity_2m,
+        weatherData.current_units.relative_humidity_2m,
+      ],
+      windSpeed: [
+        weatherData.current.wind_speed_10m,
+        weatherData.current_units.wind_speed_10m,
+      ],
+    };
+  } catch (err) {
+    console.log("something bad happened: ", err);
+  }
+}
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  weatherInformation(input.value);
+});

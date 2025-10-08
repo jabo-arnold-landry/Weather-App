e6062ccef81fbe34e839bf800ff430bae237eb2a
jabo arnold landry <arnoldjab@gmail.com>
2025-10-08 18:34:24 +0200

    feat:populate-daily-forecast-section
    I populated the weekly forecast for each day to have its minmum and maximum temperature

--- src/javascript/cardsDisplay.js
+++ src/javascript/cardsDisplay.js
@@ -1,8 +1,9 @@
 const generalInfoSection = document.querySelector("#general-info-section");
+const dailyForecastSection = document.querySelector("#daily-forecast-section");
 
 function displayGeneralWeatherInformation(obj) {
-  generalInfoSection.innerHTML = "";
   const docFragment = document.createDocumentFragment();
+  generalInfoSection.innerHTML = "";
   for (let keys in obj) {
     const div = document.createElement("div");
     const weatherValue = obj[keys][0];
@@ -15,6 +16,29 @@ function displayGeneralWeatherInformation(obj) {
   }
   generalInfoSection.append(docFragment);
 }
-
-function sevenDaysForecastDisplay() {}
-export { displayGeneralWeatherInformation };
+const daysofWeek = [
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+  "sunday",
+];
+function sevenDaysForecastDisplay(maxWeekForecasts, minWeekForecasts) {
+  let docFragment = document.createDocumentFragment();
+  dailyForecastSection.innerHTML = "";
+  for (let i = 0; i < 7; i++) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <h3>${daysofWeek[i]}</h3>
+    <img src='' alt="weather status image" >
+    <section>
+      <span>${maxWeekForecasts[i]}<sup>o</sup></span>
+      <span>${minWeekForecasts[i]}<sup>o</sup></span>
+    </section>`;
+    docFragment.append(div);
+  }
+  dailyForecastSection.append(docFragment);
+}
+export { displayGeneralWeatherInformation, sevenDaysForecastDisplay };

c76da2bf9d4a5fc9a82d3d3ccfd909a3ae3bb3f8
jabo arnold landry <arnoldjab@gmail.com>
2025-10-08 17:35:46 +0200

    fix:add-readable-variables
    I added a clear definition for indicating the weather value and its unit in a more readable way than before, the code before this commit well hard to read and understand what are they doing and maintaining them would be hard that is why i changed their names to make sense of them

--- src/javascript/cardsDisplay.js
+++ src/javascript/cardsDisplay.js
@@ -5,12 +5,18 @@ function displayGeneralWeatherInformation(obj) {
   const docFragment = document.createDocumentFragment();
   for (let keys in obj) {
     const div = document.createElement("div");
+    const weatherValue = obj[keys][0];
+    const valueUnit = obj[keys][1]
     div.innerHTML += `
       <h2>${keys}</h2
-      <p>${obj[keys][0]} ${obj[keys][1]}</p>
+      <p>${weatherValue} ${valueUnit}</p>
     `;
     docFragment.append(div);
   }
   generalInfoSection.append(docFragment);
+}
+
+function sevenDaysForecastDisplay(){
+  
 }
 export { displayGeneralWeatherInformation };

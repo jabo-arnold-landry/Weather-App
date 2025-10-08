a314ec17edcd0f81d06070ab433edc172c832704
jabo arnold landry <arnoldjab@gmail.com>
2025-10-08 20:13:31 +0200

    feat:add-the-icon-list-file-to-handle-weather-data

---
+++ src/javascript/iconsList.js
@@ -0,0 +1,24 @@
+function getWeatherIcon(weatherCode, isDay) {
+  const icons = {
+    0: { day: "/images/", night: "clear-night.svg" },
+    1: { day: "clear.svg", night: "clear-night.svg" },
+    2: { day: "/images/partly-cloudy.webp", night: "partly-cloudy-night.svg" },
+    3: { day: "/images/overcast.webp", night: "overcast.svg" },
+    45: { day: "/images/fog.svg", night: "fog.svg" },
+    48: { day: "fog.svg", night: "fog.svg" },
+    51: { day: "drizzle.svg", night: "drizzle.svg" },
+    53: { day: "drizzle.svg", night: "drizzle.svg" },
+    55: { day: "drizzle.svg", night: "drizzle.svg" },
+    61: { day: "rain.svg", night: "rain.svg" },
+    63: { day: "rain.svg", night: "rain.svg" },
+    65: { day: "rain.svg", night: "rain.svg" },
+    80: { day: "showers.svg", night: "showers.svg" },
+    81: { day: "showers.svg", night: "showers.svg" },
+    82: { day: "showers.svg", night: "showers.svg" },
+    95: { day: "thunderstorm.svg", night: "thunderstorm.svg" },
+    96: { day: "thunderstorm.svg", night: "thunderstorm.svg" },
+    99: { day: "thunderstorm.svg", night: "thunderstorm.svg" },
+  };
+
+ 
+} 
